eab77fc5ed08312025a24af7f9b61ee81025c590
mcdott <111994803+mcdott@users.noreply.github.com>
2023-04-16 15:55:51 -0400

    Added Sliders component with basic responsive layout for the simulations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./components/Hero";
+import Sliders from "./components/Sliders";
 
 function App() {
   return (
     <div>
       <Hero />
+      <Sliders />
     </div>
   );
 }
