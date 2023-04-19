fb37f578681281217645192e35e26ab708d5d687
mcdott <111994803+mcdott@users.noreply.github.com>
2023-04-18 21:09:43 -0400

    Fixed navbar links to display on correct pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,50 @@
 // Navbar.tsx
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 interface NavbarProps {
   scrollToSliders: () => void;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ scrollToSliders }) => {
+  const location = useLocation();
+
   return (
     <nav className='bg-blue-500 p-4 text-white font-bold'>
       <ul className='flex items-center justify-between'>
         <li className='text-2xl'>Hemianopsia Sims</li>
         <li>
           <ul className='flex items-center space-x-4'>
-            <li>
-              <button
-                className='focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 transition-colors duration-300'
-                onClick={scrollToSliders}
-              >
-                Simulations
-              </button>
-            </li>
-            <li>
-              <Link
-                to='/about'
-                className='focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 transition-colors duration-300'
-              >
-                About
-              </Link>
-            </li>
+            {location.pathname === "/about" ? (
+              <li>
+                <Link
+                  to='/'
+                  className='focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 transition-colors duration-300'
+                >
+                  Home
+                </Link>
+              </li>
+            ) : (
+              <>
+                <li>
+                  <button
+                    className='focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 transition-colors duration-300'
+                    onClick={scrollToSliders}
+                  >
+                    Simulations
+                  </button>
+                </li>
+                <li>
+                  <Link
+                    to='/about'
+                    className='focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 transition-colors duration-300'
+                  >
+                    About
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </li>
       </ul>
