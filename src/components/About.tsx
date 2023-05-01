21ab546f59875d14c16bee787e5894e81f020f9e
mcdott <111994803+mcdott@users.noreply.github.com>
2023-05-01 15:15:35 -0400

    Added testimonials to About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,43 @@
-// src/About.tsx
-
 import React from "react";
 
 const About: React.FC = () => {
   return (
-    <div className='p-8'>
-      <h1 className='text-3xl mb-4'>About</h1>
-      <p className='text-lg'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-        odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
-        quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
-        mauris.
-      </p>
+    <div className='container mx-auto px-4 py-12'>
+      <div className='text-center'>
+        <h1 className='text-3xl lg:text-4xl font-bold mb-8'>About</h1>
+        <p className='text-lg mx-auto max-w-3xl mb-12'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+          odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+          quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
+          mauris.
+        </p>
+      </div>
+
+      <div className='bg-blue-50 p-6 rounded-lg mx-auto max-w-3xl mb-6'>
+        <h2 className='text-2xl font-semibold mb-6'>Testimonials</h2>
+        <blockquote className='text-lg mb-6'>
+          <p className='mb-4'>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+            Nulla quis sem at nibh elementum imperdiet."
+          </p>
+          <footer className='font-semibold'>
+            &mdash; Pia Chowdhury, daughter of a person with hemianopsia
+          </footer>
+        </blockquote>
+      </div>
+      <div className='bg-blue-50 p-6 rounded-lg mx-auto max-w-3xl mb-6'>
+        <blockquote className='text-lg mb-6'>
+          <p className='mb-4'>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+            Nulla quis sem at nibh elementum imperdiet."
+          </p>
+          <footer className='font-semibold'>
+            &mdash; Sylvain, friend of a person with hemianopsia
+          </footer>
+        </blockquote>
+      </div>
     </div>
   );
 };
