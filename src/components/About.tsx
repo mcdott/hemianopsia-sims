823bbc0e6fee1e4f8aff87c52c1ccfd5811b01da
mcdott <111994803+mcdott@users.noreply.github.com>
2023-11-04 17:24:58 -0400

    Replaces tailwind styles with css in About, DrawButton, and Hemianopsia

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./About.css";
 
 const About: React.FC = () => {
   return (
-    <main className='container mx-auto px-4 py-12 text-neutral-900'>
-      <section className='text-center'>
-        <h1 className='text-3xl lg:text-4xl font-bold mb-8'>About</h1>
-        <p className='text-lg mx-auto max-w-3xl mb-12'>
+    <main className='about-container'>
+      <section className='about-section'>
+        <h1 className='about-heading'>About</h1>
+        <p className='about-paragraph'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
           odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
           quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
@@ -13,29 +14,27 @@ const About: React.FC = () => {
         </p>
       </section>
       <section>
-        <h2 className='text-center text-2xl font-semibold mb-6'>
-          Testimonials
-        </h2>
-        <figure className='bg-tertiary p-6 rounded-lg mx-auto max-w-3xl mb-6'>
-          <blockquote className='text-md mb-6'>
+        <h2 className='about-subheading'>Testimonials</h2>
+        <figure className='about-figure'>
+          <blockquote className='about-blockquote'>
             <p className='mb-4'>
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
               nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
               Nulla quis sem at nibh elementum imperdiet."
             </p>
-            <figcaption className='font-semibold'>
+            <figcaption className='about-figcaption'>
               &mdash; Pia Chowdhury, daughter of a person with hemianopsia
             </figcaption>
           </blockquote>
         </figure>
-        <figure className='bg-tertiary p-6 rounded-lg mx-auto max-w-3xl mb-6'>
-          <blockquote className='text-md mb-6'>
+        <figure className='about-figure'>
+          <blockquote className='about-blockquote'>
             <p className='mb-4'>
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
               nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
               Nulla quis sem at nibh elementum imperdiet."
             </p>
-            <figcaption className='font-semibold'>
+            <figcaption className='about-figcaption'>
               &mdash; Sylvain, friend of a person with hemianopsia
             </figcaption>
           </blockquote>
